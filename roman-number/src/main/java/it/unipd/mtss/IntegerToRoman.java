73928ba13ec1f7808cd43e02d877393586d59443
marameo0 <grossellediana@gmail.com>
2026-05-07 11:18:53 +0200

    fix: fallimento checkstyle per indentazione e spazi

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -2,12 +2,11 @@
 // [DIANA] [GROSSELLE] [2137985]
 // [VALENTINO NICOLA] [MILANI] [2147991]
 ////////////////////////////////////////////////////////////////////
-
 package it.unipd.mtss;
 
-public class IntegerToRoman { 
+public class IntegerToRoman {
 
-    public static String convert(int number){
+    public static String convert(int number) {
         // TODO
         return null;
     }
--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -2,16 +2,16 @@
 // [DIANA] [GROSSELLE] [2137985]
 // [VALENTINO NICOLA] [MILANI] [2147991]
 ////////////////////////////////////////////////////////////////////
-
 package it.unipd.mtss;
 
 public class RomanPrinter {
-  public static String print(int num){
-  return printAsciiArt(IntegerToRoman.convert(num));
-  }
-  
-  private static String printAsciiArt(String romanNumber){
-  //TODO
-  return null;
-  }
+
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        // TODO
+        return null;
+    }
 }
