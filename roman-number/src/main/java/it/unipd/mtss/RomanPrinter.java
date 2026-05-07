4606277d77ba1c3dabb9ad13e842c9225e74be25
mvale2066 <milani.valentino.22@gmail.com>
2026-05-07 16:07:44 +0200

    Implementazione della stampa fino a 100

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -43,6 +43,15 @@ public class RomanPrinter {
         "| |____  ",
         "|______| "
     };
+    // ASCII art della lettera C (100)
+    private static final String[] ASCII_C = {
+        "  _____  ",
+        " / ____| ",
+        "| |      ",
+        "| |      ",
+        "| |____  ",
+        " \\_____| "
+    };
 
     public static String print(int n) {
         return printAsciiArt(IntegerToRoman.convert(n));
@@ -76,6 +85,7 @@ public class RomanPrinter {
             case 'V': return ASCII_V;
             case 'X': return ASCII_X;
             case 'L': return ASCII_L;
+            case 'C': return ASCII_C;
             default: throw new IllegalArgumentException(
                 "Carattere non supportato: " + c);
         }
